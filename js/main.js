3505649d4278df5e0954902fee2ee46e626823b7
Fabio <fabio.mezzina.09@gmail.com>
2020-11-10 11:12:34 +0100

    JS - add element

--- js/main.js
+++ js/main.js
@@ -6,10 +6,18 @@ const todo = new Vue({
       'Fare la spesa',
       'Pagare le bollette'
     ],
+    newElement: '',
   },
   methods: {
+    // remove the element with click on X
     removeElement(index) {
       this.list.splice(index,1);
+    },
+    // add an element if the input tag is not empty
+    addElement() {
+      if(this.newElement.trim() !== '') {
+        this.list.push(this.newElement.trim());
+      }
     }
   }
 });
